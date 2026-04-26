11400aca17cd6aefb2c65ec92d8ef370e256593e
blueycoura04 <dewi.ara2011@gmail.com>
2026-04-26 14:05:59 +0700

    Update hapus_artikel.php

--- projectblog/penulis/hapus_artikel.php
+++ projectblog/penulis/hapus_artikel.php
@@ -1,13 +1,32 @@
 <?php
-include "../../koneksi.php";
+session_start();
+include "../koneksi.php";
 
-$id = $_GET['id'];
+if(!isset($_SESSION['login'])){
+    header("Location: ../login.php");
+    exit;
+}
+
+$id = (int) $_GET['id'];
 $username = $_SESSION['username'];
 
-/* pastikan hanya milik sendiri */
-mysqli_query($conn, "
-    DELETE FROM artikel 
-    WHERE id_artikel='$id' AND penulis='$username'
-");
+/* ambil id_user */
+$stmt = mysqli_prepare($conn, "SELECT id_user FROM users WHERE username=?");
+mysqli_stmt_bind_param($stmt, "s", $username);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
+$user = mysqli_fetch_assoc($result);
 
-header("Location: ../index.php?menu=artikel_saya");
+$id_user = $user['id_user'];
+
+/* hapus berdasarkan id_user */
+$stmt = mysqli_prepare($conn, "
+    DELETE FROM artikel 
+    WHERE id_artikel=? AND id_user=?
+");
+mysqli_stmt_bind_param($stmt, "ii", $id, $id_user);
+mysqli_stmt_execute($stmt);
+
+header("Location: index.php?menu=artikel_saya");
+exit;
+?>
